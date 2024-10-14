28754fc206b576fc7bec1e54c7f4c7f5dd86574c
imightbuyaboat <vovkbp@gmail.com>
2024-10-14 13:59:07 +0300

    2.0

--- lab4/tree.h
+++ lab4/tree.h
@@ -1,20 +1,29 @@
 #include <iostream>
+#include <cstdint>
 
-struct node {
+struct Node {
     int32_t value;
-    node* left;
-    node* right;
+    Node* left;
+    Node* right;
+
+    Node(int32_t _value) : value(_value), left(nullptr), right(nullptr) {};
 };
 
 class Tree
 {
 private:
-    node* root;
+    Node* root;
     int32_t count = 0;
+
+    Node* InsertNode(Node* node, int32_t x);
+    bool SearchNode(Node* node, int32_t x);
+    void SearchXMinNode(Node* node, int32_t* min, int32_t& ammount);
 public:
     Tree(const char* fileName);
     ~Tree();
-    int Add(int32_t x);
+    bool Insert(int32_t x);
+    bool Search(int32_t x);
+    int* SearchXMin(int32_t ammount);
 };
 
 Tree::Tree(const char* fileName) {
@@ -24,38 +33,74 @@ Tree::Tree(const char* fileName) {
 Tree::~Tree() {
 }
 
-int Tree::Add(int32_t x) {
-    node* newNode = new node;
-    newNode->value = x;
-    newNode->left = nullptr;
-    newNode->right = nullptr;
-
-    if (!root) {
-        root = newNode;
-        return 1;
-    }
-    
-    node* curr = root;
-    while(1) {
-        if(x < curr->value) {
-            if(!curr->left) {
-                curr->left = newNode;
-                break;
-            } 
-            else curr = curr->left; 
-        }
-        else if(x > curr->value) {
-            if(!curr->right) {
-                curr->right = newNode;
-                break;
-            } 
-            else curr = curr->right; 
-        }
-        else {
-            delete newNode;
-            return 0;
-        }
+Node* Tree::InsertNode(Node* node, int32_t x) {
+    if(!node) {
+        count++;
+        return new Node(x);
     }
 
-    return 1;
+    if(x < node->value) {
+        node->left = InsertNode(node->left, x);
+    }
+    else if(x > node->value) {
+        node->right = InsertNode(node->right, x);
+    }
+    else {
+        return node;
+    }
+
+    return node;
+}
+
+bool Tree::Insert(int32_t x) {
+    root = InsertNode(root, x);
+    return root != nullptr;
+}
+
+bool Tree::SearchNode(Node* node, int32_t x) {
+    if(!node) return false;
+
+    if(node->value == x) {
+        return true;
+    }
+
+    if(x < node->value) {
+        return SearchNode(node->left, x);
+    }
+    else {
+        return SearchNode(node->right, x);
+    }
+}
+
+bool Tree::Search(int32_t x) {
+    return SearchNode(root, x);
+}
+
+void Tree::SearchXMinNode(Node* node, int32_t* min, int32_t& ammount) {
+    if(!node) return;
+
+    SearchXMinNode(node->left, min, ammount);
+
+    int32_t maxIndex = 0;
+    for(int32_t i = 1; i < ammount; i++) {
+        if(min[i] > min[maxIndex]) {
+            maxIndex = i;
+        }
+    }
+
+    if(node->value < min[maxIndex]) {
+        min[maxIndex] = node->value;
+    }
+
+    SearchXMinNode(node->right, min, ammount);
+}
+
+int* Tree::SearchXMin(int32_t ammount) {
+    if(count < ammount) return nullptr;
+
+    int32_t* min = new int32_t[ammount];
+    for(size_t i = 0; i < ammount; i++) min[i] = INT32_MAX;
+
+    SearchXMinNode(root, min, ammount);
+    return min;
 }
