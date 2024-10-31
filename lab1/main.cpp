0dce96cae45579bf6f77404721c4a88d44b2b387
imightbuyaboat <vovkbp@gmail.com>
2024-10-31 13:26:54 +0300

    5.1

--- lab1/main.cpp
+++ lab1/main.cpp
@@ -14,35 +14,10 @@
 char* outputFileName = "default.txt";
 const int16_t SRC = 0;
 
-void createGraph(const int16_t size, const char* fileName) {
-    std::ofstream file(fileName, std::ios::binary);
-
-    file.write(reinterpret_cast<const char*>(&size), sizeof(int16_t));
-
-    srand(static_cast<unsigned int>(time(0)));
-    for (int16_t i = 0; i < size; i++) {
-        for (int16_t j = 0; j < size; j++) {
-            //int16_t value = rand() % (2 * INT16_MAX + 1) - INT16_MAX;
-            //int16_t value = rand() % (2 * 20 + 1) - 20;
-
-            int16_t value = rand() % 21;
-            file.write(reinterpret_cast<const char*>(&value), sizeof(int16_t));
-        }
-    }
-
-    file.close();
-}
-
 int main(int argc, char* argv[]) {
     if (argc != 2 && argc != 4) {
         std::cerr << "Incorrect number of arguments: " << argc << std::endl;
         return 1;
-    }
-
-    if (strcmp(argv[1], "-c") == 0) {
-        int16_t size = static_cast<int16_t>(std::stoi(argv[3]));
-        createGraph(size, argv[2]);
-        return 0;
     }
 
     //меняем название выходного файла
