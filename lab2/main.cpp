722704176e4c78fb2af1668e822c422f2a3e97ca
imightbuyaboat <vovkbp@gmail.com>
2024-10-31 13:31:35 +0300

    4.0

--- lab2/main.cpp
+++ lab2/main.cpp
@@ -12,43 +12,17 @@
 
 char* outputFileName = "default.txt";
 
-//const int16_t SRC = 376, END = 309;     //1
-// const int16_t SRC = 168, END = 267;     //2
+const int16_t SRC = 376, END = 309;     //1
+//const int16_t SRC = 168, END = 267;     //2
 //const int16_t SRC = 340, END = 995;     //3
-//const int16_t SRC = 477, END = 231;     //4 -
+//const int16_t SRC = 477, END = 231;     //4
 //const int16_t SRC = 637, END = 471;     //5
-const int16_t SRC = 93, END = 604;      //6 -
-
-void createGraph(const int16_t size, const char* fileName) {
-    std::ofstream file(fileName, std::ios::binary);
-
-    file.write(reinterpret_cast<const char*>(&size), sizeof(int16_t));
-
-    srand(static_cast<unsigned int>(time(0)));
-    for (int16_t i = 0; i < size; i++) {
-        for (int16_t j = 0; j < size; j++) {
-            int16_t value;
-            if(i == j) value = 0;
-            else value = rand() % 21;
-            file.write(reinterpret_cast<const char*>(&i), sizeof(int16_t));
-            file.write(reinterpret_cast<const char*>(&j), sizeof(int16_t));
-            file.write(reinterpret_cast<const char*>(&value), sizeof(int16_t));
-        }
-    }
-
-    file.close();
-}
+//const int16_t SRC = 93, END = 604;      //6
 
 int main(int argc, char* argv[]) {
     if (argc != 2 && argc != 4) {
         std::cerr << "Incorrect number of arguments: " << argc << std::endl;
         return 1;
-    }
-
-    if (strcmp(argv[1], "-c") == 0) {
-        int16_t size = static_cast<int16_t>(std::stoi(argv[3]));
-        createGraph(size, argv[2]);
-        return 0;
     }
 
     if (argc == 4) {
